43dc208d33e8d925af5e850a2c97f92dc79cd8e0
Umberto Palazzo <upalazzo93@gmail.com>
2019-08-16 20:59:25 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,6 +3,11 @@ def my_collection(array)
   new_collection = []
   while i < array.length 
     new_collection << yield(array[i]) 
+    i+=1
   end 
 end 
 
+my_collection("home", "corn", "beer") do |mod|
+  mod.upcase
+end 
+  
